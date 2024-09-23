86dac1bbbc60a9f4b911cc8b18d828cd603e7237
aquinoalejandro <aquinoalejandro666@gmail.com>
2024-09-22 22:30:24 -0300

    feat: :sparkles: Adaptador y Observer implementados

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import { Inventario } from "./patrones/singleton/singleton";
 import { EquipoFactory } from "./patrones/factoryMethod/factoryMethod";
+import { Equipo, Soporte } from "./patrones/observer/observer";
+import { InventarioViejo, AdaptadorInventario } from "./patrones/adaptador/adaptador"; 
 
 
 //Ejercicio 1 
@@ -13,4 +15,22 @@ console.log(inventario.listarEquipos());
 const factory = new EquipoFactory();
 const Notebook = factory.crearEquipo("Notebook", "Dell XPS", "16GB", "i7");
 console.log(Notebook.detalles());
+// Resultado esperado:
 // Tipo: Notebook, Nombre: Dell XPS, RAM: 16GB, Procesador: i7
+
+
+const soporte = new Soporte();
+const equipo = new Equipo("Notebook HP", "Portátil", "disponible");
+console.log(equipo)
+
+soporte.agregarObservador(equipo);
+soporte.cambiarEstado("en reparación");
+// Resultado esperado:
+// Soporte notificado: Notebook HP ha cambiado su estado a en reparación.
+
+const inventarioViejo = new InventarioViejo();
+const adaptador = new AdaptadorInventario(inventarioViejo);
+adaptador.agregarEquipo("Servidor Dell", "Servidor", "disponible");
+console.log(adaptador.listarEquipos());
+// Resultado esperado: 
+// [{ nombre: "Servidor Dell", tipo: "Servidor", estado: "disponible" }]
